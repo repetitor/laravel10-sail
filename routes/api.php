74533306e7e18654190d6f14c67bb908ab590f39
Viktar Akulenka <repetitor202@gmail.com>
2023-04-14 18:21:30 +0300

    try

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+const ENTITY = 'tests';
+const CHILDREN = 'test-children';
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +20,38 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('test', function () {
+    return 'test202';
+});
+
+Route::prefix(ENTITY)->group(function () {
+    Route::get('', function () {return ENTITY;});
+    // ...CRUD1
+
+    Route::prefix('{id}')->group(function () {
+        Route::get('', function (int $id) {return ENTITY . '/' . $id;});
+        // ...CRUD2
+
+        Route::prefix(CHILDREN)->group(function () {
+            Route::get('', function ($id) {return ENTITY . '/' . $id . '/' . CHILDREN;});
+            // ...CRUD3
+
+            Route::get('{idChild}', function (int $idChild, int $id) {return ENTITY . '/' . $id . '/' . CHILDREN . '/' . $idChild;});
+            // ...CRUD4
+
+//            Route::prefix('{idChild}')->group(function () {
+////                Route::get('', function (int $idChild, int $id) {return ENTITY . '/' . $id . '/' . CHILDREN . '/' . $idChild;});
+//                Route::get('', function () {return url()->current();});
+//                // ...CRUD4
+//            });
+        });
+    });
+});
+//Route::prefix(CHILDREN. '/{id}/' . CHILDREN . '/{idChild}')->group(function () {
+//    Route::get('', function (int $idChild, int $id) {
+////        return ENTITY . '/' . $id . '/' . CHILDREN . '/' . $idChild;
+////        return url()->current();
+//    });
+//    // ...CRUD4
+//});
